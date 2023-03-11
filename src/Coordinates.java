17d68e3d585d99f1e05ed5b4683500ce5198e003
jack <jaksonmillka@gmail.com>
2023-03-11 14:03:38 +0200

    added class = Coordinates. In class added variable rank, and created constructor

---
+++ src/Coordinates.java
@@ -0,0 +1,10 @@
+public class Coordinates {
+
+    public final Designation designation;
+    public final Integer rank;
+    public Coordinates(Designation designation, Integer rank) {
+        this.designation = designation;
+        this.rank = rank;
+    }
+
+}
