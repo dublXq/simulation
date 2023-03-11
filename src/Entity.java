461f15815ca579c1cf423478c1f990e9445568fd
jack <jaksonmillka@gmail.com>
2023-03-11 12:30:43 +0200

    added the main class Launcher, and a class Entity for objects

---
+++ src/Entity.java
@@ -0,0 +1,7 @@
+public class Entity {
+    public final Object DEVELOPER = "\uD83D\uDC68\u200D\uD83D\uDCBB";
+    public final Object BUGS = "\uD83D\uDC1B";
+    public final Object TESTER = "\uD83C\uDFC3";
+    public final Object BORDER = "\uD83D\uDDC4️";
+    public final Object FIELD_BOUNDARY = "█";
+}
