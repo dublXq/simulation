87c6044a6e84bcba1c46381c31a3b764811d1761
jack <jaksonmillka@gmail.com>
2023-03-12 10:37:15 +0200

    added method addPersonage in class Board

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,11 @@
+import java.util.HashMap;
+
 public class Board {
+    HashMap<Coordinates, Personage> personages = new HashMap<>();
+
+    public void addPersonage(Coordinates coordinates, Personage personage) {
+        personage.coordinates = coordinates;
+        personages.put(coordinates, personage);
+    }
 
 }
