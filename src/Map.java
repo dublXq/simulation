f2ef742b42a1073ee3c4b47fc18e42793c00d8b9
jack <jaksonmillka@gmail.com>
2023-03-12 10:55:51 +0200

    fix: deleted class Board. Change classes in Map

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 public class Map {
-
-
-
+    HashMap<Coordinates, Personage> personages = new HashMap<>();
+    public void addPersonage(Coordinates coordinates, Personage personage) {
+        personage.coordinates = coordinates;
+        personages.put(coordinates, personage);
+    }
+    public void removePersonage(Coordinates coordinates, Personage personage) {
+        personage.coordinates = coordinates;
+        personages.remove(coordinates, personage);
+    }
 }
