bf2e81526e86d63d0b845be529096b2f2bef0359
jack <jaksonmillka@gmail.com>
2023-03-12 09:58:11 +0200

    fix: added static label to the all variable

--- src/Entity.java
+++ src/Entity.java
@@ -1,7 +1,7 @@
-public class Entity {
-    public final Object DEVELOPER = "\uD83D\uDC68\u200D\uD83D\uDCBB";
-    public final Object BUGS = "\uD83D\uDC1B";
-    public final Object TESTER = "\uD83C\uDFC3";
-    public final Object BORDER = "\uD83D\uDDC4️";
-    public final Object BRICK = "\uD83E\uDDF1";
+public abstract class Entity {
+    public static final Object DEVELOPER = "\uD83D\uDC68\u200D\uD83D\uDCBB";
+    public static final Object BUGS = "\uD83D\uDC1B";
+    public static final Object TESTER = "\uD83C\uDFC3";
+    public static final Object BORDER = "\uD83D\uDDC4️";
+    public static final Object BRICK = "\uD83E\uDDF1";
 }
